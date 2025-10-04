14549676fd7ac76d20c06ffe2ada687f6a025ead
Carson Jan <sc.carson.jan@gmail.com>
2025-10-04 13:53:36 -0700

    add data struct and function signature

---
+++ objects/Posting.js
@@ -0,0 +1,10 @@
+class Posting {
+    constructor(title, image, price, tags) {
+        this.title = title;
+        this.image = image;
+        this.price = price;
+        this.tags = tags;
+    }
+}
+
+export default Posting;
---
+++ objects/User.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(id, name, tags) {
+        this.id = id;
+        this.name = name;
+        this.tags = tags;
+    }
+}
+
+export default User;
